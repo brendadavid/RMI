f87a7ac1abd8a0c2e0a751899333355a482f0101
Willian Alves <wba.alves@gmail.com>
2020-05-25 19:55:04 -0300

    implement Jogo

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,16 +1,19 @@
 package src;
 
+import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.ServerNotActiveException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Random;
 import java.rmi.registry.LocateRegistry;
 
 public class Jogo extends UnicastRemoteObject implements JogoInterface {
   private static final long serialVersionUID = 2520268900833408151L;
-  private static volatile int result;
-  private static volatile boolean changed;
   private static String clientHost = "localhost";
+
+  private final Random random = new Random();
 
   public Jogo() throws RemoteException {}
 
@@ -34,53 +37,45 @@ public class Jogo extends UnicastRemoteObject implements JogoInterface {
     } catch (Exception e) {
       System.out.println("Server failed: " + e);
     }
-
-    while (true) {
-      if (changed == true) {
-        changed = false;
-
-        String connectLocation = "//" + clientHost + "/Jogador";
-
-        JogadorInterface jogador = null;
-        try {
-          System.out.println("Connecting to client at : " + connectLocation);
-          jogador = (JogadorInterface) Naming.lookup(connectLocation);
-        } catch (Exception e) {
-          System.out.println ("Client failed: ");
-          e.printStackTrace();
-        }
-
-        try {
-          jogador.cutucado();
-        } catch (RemoteException e) {
-          e.printStackTrace();
-        }
-      }
-      try {
-        Thread.sleep(100);
-      } catch (InterruptedException ex) {}
-    }
   }
 
   public int registra() {
-    System.out.println("Registro!");
-    return 1;
+    int clientId = random.nextInt(100);
+    System.out.println(String.format("Registro do cliente %d!", clientId));
+    return clientId;
   }
 
   public int joga(int id) {
-    while (changed == true);
-    changed = true;
-
-    try {
-      clientHost = getClientHost();
-    } catch (ServerNotActiveException e) {
-      e.printStackTrace();
-    }
-
-    return 2;
+      try {
+        clientHost = getClientHost();
+        String connectLocation = "//" + clientHost + "/Jogador";
+        JogadorInterface jogador = (JogadorInterface) Naming.lookup(connectLocation);
+        int chance = random.nextInt(100);
+        System.out.println(String.format("Valor obtido: %d", chance));
+        if (chance <= 0) {
+          jogador.encerrado();
+          System.out.println(String.format("Jogador %d foi encerrado!", id));
+        } else if (chance > 0 && chance <= 20) {
+          jogador.cutucado();
+          System.out.println(String.format("Jogador %d foi cutucado!", id));
+        } else {
+          System.out.println(String.format("Jogador %d jogou!", id));
+        }
+      } catch (MalformedURLException | RemoteException | NotBoundException | ServerNotActiveException e) {
+        e.printStackTrace();
+      }
+    return id;
   }
 
   public int encerra(int id) {
-    return 3;
+    String connectLocation = "//" + clientHost + "/Jogador";
+    try {
+      JogadorInterface jogador = (JogadorInterface) Naming.lookup(connectLocation);
+      jogador.encerrado();
+      System.out.println(String.format("Jogador %d foi encerrado!", id));
+    } catch (MalformedURLException | RemoteException | NotBoundException e) {
+      e.printStackTrace();
+    }
+    return id;
   }
 }
