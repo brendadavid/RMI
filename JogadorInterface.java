068998908a938a9a5b0173ac4cadb54fa6bd2f88
BrendaDavid <42557879+brendadavid@users.noreply.github.com>
2020-05-25 16:34:18 -0300

    Add files via upload

---
+++ JogadorInterface.java
@@ -0,0 +1,7 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface JogadorInterface extends Remote {
+	public void encerrado() throws RemoteException;
+	public void cutucado() throws RemoteException;
+}
